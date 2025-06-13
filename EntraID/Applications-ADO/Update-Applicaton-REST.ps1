997610750ed90964467fae7f6e3d73cfb6075cc0
System Administrator <admin@entraidlab.com>
2025-06-13 18:35:17 +0000

    Updated Update-Applicaton-REST.ps1

--- EntraID/Applications-ADO/Update-Applicaton-REST.ps1
+++ EntraID/Applications-ADO/Update-Applicaton-REST.ps1
@@ -99,14 +99,8 @@ Write-Host "path $JsonFile"
 $existingStateJson = Get-content -Path $JsonFile -RAW
 $existingStateObj = $existingStateJson | ConvertFrom-Json
 $existingStateObj.PSObject.Properties.Remove('@odata.context')
-write-host "Json content of the input file object:"
-Write-host "$existingStateObj"
 write-host "Json content of the input file:"
 Write-host $existingStateJson
-$URI = 'https://graph.microsoft.com/beta/applications/'+"$($existingStateObj.id)"
-Write-host "$($existingStateObj.id)"
-Write-host "URL - $URI"
-throw error
 $GUID = $(New-Guid).Guid
 write-host "Using $GUID for Service Principal's notes property for verification" -ForegroundColor Green
 $newAppStateObj = Get-content -Path $newJsonFile -RAW | ConvertFrom-Json
